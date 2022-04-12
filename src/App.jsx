8c4d413f12c357c14c16b52590c3112ea2251351
Piotr-Zacharski <zacharski.p.h@gmail.com>
2022-04-12 16:11:29 +0200

    Own API

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import { useState } from 'react'
+import { Paper } from '@mui/material'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [films, setFilms] = useState([])
+
+  const getFilms = () => {
+    fetch('https://my-film-api.herokuapp.com/films')
+      .then(response => response.json())
+      .then(data => {
+        setFilms(data)
+      }
+      )
+  }
+
+  useEffect(() => {
+    getFilms()
+  },[])
 
   return (
-    <div className="App">
-      
+    <div>
+      {films.map(film => (
+        <Paper key={film.id} elevation={24} style={{width: '800px', textAlign: 'center', justifyContent: 'center', margin: '0 auto'}}>
+          <h1>{film.title}</h1>
+          <p>{film.director}</p>
+          <p>{film.actors}</p>
+          <p>{film.plot}</p>
+          <p>{film.year}</p>
+        </Paper>
+      ))}
     </div>
   )
 }
